5ecd0fbaae7d71c814c52f87a625c29fcd105169
rabi <wrabwagle123@gmail.com>
2023-04-30 21:31:46 +0545

    first

---
+++ R programming.R
@@ -0,0 +1,44 @@
+# Fizzbuzz
+for (i in 1:20)
+{
+  if (i%%15==0)
+    cat("FIZZBUZZ ")
+  else if (i%%3==0)
+    cat("fizz ")
+  else if (i%%5==0)
+    cat("buzz ")
+  else
+    cat(i," ")
+}
+
+n<-10
+sum=0
+for(i in 1:n){
+  sum=sum+i
+  print(sum)
+  
+  
+}
+
+n= as.integer(readline("enter the number"))
+nu=as.integer(readline("enter the secobd numer"))
+c=n+nu
+
+print(c)
+
+n=13
+ap=n*(n+1)/2
+print(ap)
+
+if(ap>sum){
+  print(ap+sum)
+}
+
+ for (i in 1:10) {
+   sum==0
+   sum=sum+i^3
+   print(summ)
+   
+   
+ }
+
